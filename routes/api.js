e9c6be86a5f5526f92dfa9bb80af4a52721c3bd0
Glitch (mahogany-bosworth) <none>
2020-01-11 18:58:33 +0000

    🛣🏉 Checkpoint ./routes/api.js:9182067/3315

--- routes/api.js
+++ routes/api.js
@@ -19,37 +19,41 @@ const connection = MongoClient.connect(process.env.MONGO_URI, {
 
 module.exports = function(app) {
   app.route("/api/stock-prices").post((req, res) => {
-    console.log(req.body);
     rp(
       "https://repeated-alpaca.glitch.me/v1/stock/" + req.body.stock + "/quote"
-    ).then(response => {
-      console.log(response)
-      let setter = {};
-      const { symbol, latestPrice } = JSON.parse(response);
-      if ("like" in req.body === false || req.body.like !== 'true') {
-        setter = {
-          $set: { price: latestPrice },
-          $setOnInsert: { stock: symbol, likes: 0 }
-        };
-      } else if (req.body.like === 'true') {
-        setter = {
-          $set: { price: latestPrice },
-          $inc: { likes: 1 },
-          $setOnInsert: { stock: symbol }
-        };
-      }
-      connection.then(client => {
-        client
-          .db("test")
-          .collection("priceChecker")
-          .findOneAndUpdate({ stock: req.body.stock.toUpperCase() }, setter, { upsert: true }).then(result => {
-          
+    )
+      .then(response => {
+        console.log(response);
+        let setter = {};
+        const { symbol, latestPrice } = JSON.parse(response);
+        if ("like" in req.body === false || req.body.like !== "true") {
+          setter = {
+            $set: { price: latestPrice },
+            $setOnInsert: { stock: symbol, likes: 0 }
+          };
+        } else if (req.body.like === "true") {
+          setter = {
+            $set: { price: latestPrice },
+            $inc: { likes: 1 },
+            $setOnInsert: { stock: symbol }
+          };
+        }
+        connection.then(client => {
+          client
+            .db("test")
+            .collection("priceChecker")
+            .findOneAndUpdate({ stock: req.body.stock.toUpperCase() }, setter, {
+              upsert: true,
+              returnOriginal: false
+            })
+            .then(result => {
+              const { _id, ...rest } = result.value;
+              res.send({ stockData: rest });
+            });
         });
+      })
+      .catch(error => {
+        return res.send("Something went wrong!");
       });
-
-      // res.send(json);
-    }).catch(error => {
-      return console.log(error)
-    });
   });
 };
