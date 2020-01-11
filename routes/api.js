4a78d732a79164c867e2ad6588cd3557b3a9e27a
Glitch (mahogany-bosworth) <none>
2020-01-11 15:44:33 +0000

    🚠🍤 Checkpoint ./routes/api.js:9182067/1131

--- routes/api.js
+++ routes/api.js
@@ -19,15 +19,27 @@ const connection = MongoClient.connect(process.env.MONGO_URI, {
 
 module.exports = function(app) {
   app.route('/api/stock-prices').post((req, res) => {
+    
     console.log(req.body)
     rp("https://repeated-alpaca.glitch.me/v1/stock/" + req.body.stock + "/quote")
       .then(response => {
+        let setter = {};
         const { symbol, latestPrice } = JSON.parse(response);
+        if('like' in req.body === false) {
+          setter = {
+            price: latestPrice
+          }
+        } else if (req.body.like === true) {
+          setter = {
+            price: latestPrice,
+            $inc: { like: 1 }
+          }
+        }
+        
         connection.then(client => {
-          client.db('test').collection('priceChecker').updateOne({ stock: req.body.stock }, )
+          client.db('test').collection('priceChecker').updateOne({ stock: req.body.stock }, { $set: setter})
         })
       
-      console.log(req.body)
       // res.send(json);
     });
   });
