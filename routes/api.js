86489c502cbaf37dd1aacc7495b7afe676dd12fa
Glitch (mahogany-bosworth) <none>
2020-01-11 20:22:32 +0000

    🌒👀 Checkpoint ./routes/api.js:9182067/964

--- routes/api.js
+++ routes/api.js
@@ -17,21 +17,28 @@ const connection = MongoClient.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 });
 
-const likeChecker () => {
+const setterMaker = (req, response) => {
+  let adress = req.header("X-Forwarded-For");
+  const { symbol, latestPrice } = JSON.parse(response);
   let setter = { };
+  let checconnection.then(client => {
+    client.db("test").collection("priceChecker").findOne({ stock: req.body.stock.toUpperCase() }).then(result => {
+      if(result === null) return false;
+    })
+  })
   if ("like" in req.body === false || req.body.like !== "true") {
     setter = {
       $set: { price: latestPrice },
-            $setOnInsert: { stock: symbol, likes: 0 }
-          };
-        } else if (req.body.like === "true") {
-          setter = {
-            $set: { price: latestPrice },
-            $inc: { likes: 1 },
-            $setOnInsert: { stock: symbol },
-            $addToSet: { ipAdresses: adress }
-          };
-        }
+      $setOnInsert: { stock: symbol, likes: 0 }
+    };
+  } else if (req.body.like === "true") {
+    setter = {
+      $set: { price: latestPrice },
+      $inc: { likes: 1 },
+      $setOnInsert: { stock: symbol },
+      $addToSet: { ipAdresses: adress }
+    };
+  }
 }
 module.exports = function(app) {
   app.route("/api/stock-prices").post((req, res) => {
@@ -39,22 +46,22 @@ module.exports = function(app) {
       "https://repeated-alpaca.glitch.me/v1/stock/" + req.body.stock + "/quote"
     )
       .then(response => {
-        let adress = req.header("X-Forwarded-For");
-        const { symbol, latestPrice } = JSON.parse(response);
         
-        connection.then(client => {
-          client
-            .db("test")
-            .collection("priceChecker")
-            .findOneAndUpdate({ stock: req.body.stock.toUpperCase() }, setter, {
-              upsert: true,
-              returnOriginal: false
-            })
-            .then(result => {
-              const { _id, ...rest } = result.value;
-              res.send({ stockData: rest });
-            });
-        });
+        
+        setterMaker(req, response);
+        // connection.then(client => {
+        //   client
+        //     .db("test")
+        //     .collection("priceChecker")
+        //     .findOneAndUpdate({ stock: req.body.stock.toUpperCase() }, setter, {
+        //       upsert: true,
+        //       returnOriginal: false
+        //     })
+        //     .then(result => {
+        //       const { _id, ...rest } = result.value;
+        //       res.send({ stockData: rest });
+        //     });
+        // });
       })
       .catch(error => {
       console.log(error)
