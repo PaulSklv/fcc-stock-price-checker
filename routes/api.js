b9e082b1b7db6ce14350f446262d990d67271e70
Glitch (mahogany-bosworth) <none>
2020-01-11 15:32:32 +0000

    😀🧀 Checkpoint ./routes/api.js:9182067/380

--- routes/api.js
+++ routes/api.js
@@ -22,9 +22,13 @@ module.exports = function(app) {
     console.log(req.body)
     rp("https://repeated-alpaca.glitch.me/v1/stock/" + req.body.stock + "/quote")
       .then(response => {
-      const { symbol, latestPrice } = JSON.parse(response);
-      console.log(json.symbol)
-      res.send(json);
+        const { symbol, latestPrice } = JSON.parse(response);
+        connection.then(client => {
+          client.db('test').collection('priceChecker').updateOne({ stock: req.body.stock }, )
+        })
+      
+      console.log(req.body)
+      // res.send(json);
     });
   });
 };
