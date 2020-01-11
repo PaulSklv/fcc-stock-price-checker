6b7f7c3a958f6f6d801a0e88aeb4ff1c2e59c500
Glitch (mahogany-bosworth) <none>
2020-01-11 12:56:35 +0000

    🎒🍡 Checkpoint ./server.js:9182067/143 ./package.json:9182067/11423 ./routes/api.js:9182067/1252

--- routes/api.js
+++ routes/api.js
@@ -1,23 +1,21 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+var expect = require("chai").expect;
+var MongoClient = require("mongodb").MongoClient;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const connection = MongoClient.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-module.exports = function (app) {
-
-  app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+module.exports = function(app) {
+  app.route("/api/stock-prices").get(function(req, res) {});
 };
