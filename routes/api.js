d85462633566d81642d4fd6000fb9f72926efc85
Glitch (mahogany-bosworth) <none>
2020-01-11 20:34:28 +0000

    🌏👑 Checkpoint ./routes/api.js:9182067/4354

--- routes/api.js
+++ routes/api.js
@@ -17,38 +17,33 @@ const connection = MongoClient.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true
 });
 
-const setterMaker = (req, response) => {
-  let adress = req.header("X-Forwarded-For");
-  const { symbol, latestPrice } = JSON.parse(response);
-  let setter = { };
-  let checconnection.then(client => {
-    client.db("test").collection("priceChecker").findOne({ stock: req.body.stock.toUpperCase() }).then(result => {
-      if(result === null) return false;
-    })
-  })
-  if ("like" in req.body === false || req.body.like !== "true") {
-    setter = {
-      $set: { price: latestPrice },
-      $setOnInsert: { stock: symbol, likes: 0 }
-    };
-  } else if (req.body.like === "true") {
-    setter = {
-      $set: { price: latestPrice },
-      $inc: { likes: 1 },
-      $setOnInsert: { stock: symbol },
-      $addToSet: { ipAdresses: adress }
-    };
-  }
-}
 module.exports = function(app) {
   app.route("/api/stock-prices").post((req, res) => {
     rp(
       "https://repeated-alpaca.glitch.me/v1/stock/" + req.body.stock + "/quote"
     )
       .then(response => {
-        
-        
-        setterMaker(req, response);
+        let adress = req.header("X-Forwarded-For");
+        const { symbol, latestPrice } = JSON.parse(response);
+        let setter = { };
+        let isIpAlreadyExists;
+        connection.then(client => {
+          client.db("test").collection("priceChecker").findOne({ stock: req.body.stock.toUpperCase() }).then(result => {
+            if ("like" in req.body === false || req.body.like !== "true") {
+              setter = {
+                $set: { price: latestPrice },
+                $setOnInsert: { stock: symbol, likes: 0 }
+              };
+            } else if (req.body.like === "true") {
+              setter = {
+                $set: { price: latestPrice },
+                $inc: { likes: 1 },
+                $setOnInsert: { stock: symbol },
+                $addToSet: { ipAdresses: adress }
+              };
+            }
+          })
+        })
         // connection.then(client => {
         //   client
         //     .db("test")
